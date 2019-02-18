581fd2e945cc1c0ef5684a790de6be2f490cdc6d
MannRobertJ <mann.robertj@gmail.com>
2019-02-18 10:14:08 +0100

    we have a moving ball

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,18 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Game from "./components/Game";
 import "./App.css";
+import { Stage } from "react-konva";
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Stage
+        className="App"
+        width={window.innerWidth}
+        height={window.innerHeight}
+      >
+        <Game />
+      </Stage>
     );
   }
 }
-
-export default App;
-
-// hi
